b7b19969577ec546b456f1f671a028261aeec9ef
fercuba96 <122713928+fercuba96@users.noreply.github.com>
2024-10-02 15:46:19 +0000

    navigation updated

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const inventoryRoute = require("./routes/inventoryRoute")
+const baseController = require("./controllers/baseController")
 
 
 /* ***********************
@@ -24,11 +26,15 @@ app.set("layout", "./layouts/layout") // not at views root
 */
 
 app.use(static)
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 //Index route
 
-app.get("/", function (req, res) { res.render("index", { title: "Home" }) })
+app.get("/", baseController.buildHome)
 
+/*app.get("/", function (req, res) { res.render("index", { title: "Home" }) })
+*/
 
 /* ***********************
  * Local Server Information
