cb834610db211cb7d1782a16bfb648c23e6c31f0
fercuba96 <fercuba96@gmail.com>
2024-10-14 20:30:10 -0500

    Changes 1

--- controllers/invController.js
+++ controllers/invController.js
@@ -19,49 +19,4 @@ invCont.buildByClassificationId = async function (req, res, next) {
     })
 }
 
-
-/*Testing*/
-/*invCont.buildByTypeId = async function (req, res, next) {
-    try {
-        const inv_id = req.params.inv_id;
-        const vehicle = await invModel.getVehiclesbyId(inv_id); // Fetch vehicle by ID
-
-        if (vehicle) {
-            const detail = utilities.buildVehicleDetail(vehicle); // Format vehicle details for view
-            let nav = await utilities.getNav();
-            res.render("./inventory/vehicle-detail", {
-                title: `${vehicle.inv_make} ${vehicle.inv_model}`, // Title displays make and model
-                nav,
-                detail,
-            });
-        } else {
-            res.status(404).send("Sorry, the vehicle could not be found.");
-        }
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-};
-
-*/
-/*Testing*/
-
 module.exports = invCont;
-
-/*
-invCont.buildByTypeId = async function (req, res, next) {
-    try {
-        const inv_id = req.params.inv_id;
-        const vehicle = await invModel.getVehicleById(inv_id); // Assuming this fetches the vehicle by ID
-
-        if (vehicle) {
-            // Pass the vehicle data to the EJS template
-            res.render('inventory/vehicle-detail', { vehicle });
-        } else {
-            res.status(404).send("Sorry, we appear to have lost that page.");
-        }
-    } catch (error) {
-        next(error);
-    }
-};
-*/
--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -4,12 +4,5 @@ const invController = require("../controllers/invController")
 // Route to build inventory by classification view
 router.get("/type/:classificationId", invController.buildByClassificationId)
 
-/*Testing*/
-/* **************************************
-* Vehicle detail route
-* ************************************ */
-/*router.get('/detail/:inv_id', invController.buildByTypeId)*/
-
-/*Testing*/
 
 module.exports = router
