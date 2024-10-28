2cdb45fd31fc3f08ffad08d1dfc5126e1383b958
fercuba96 <fercuba96@gmail.com>
2024-10-28 00:03:22 -0500

    Password fixed

--- public/js/update.js
+++ public/js/update.js
@@ -1,10 +1,10 @@
 const togglePassword = document.querySelector('#togglePassword')
-    const passwordInput = document.querySelector('#password')
+    const passwordInput = document.querySelector('#account_password')
 
     togglePassword.addEventListener('click', function (e) {
-        e.preventDefault(); 
+        e.preventDefault()
         const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password'
-        passwordInput.setAttribute('type', type);
+        passwordInput.setAttribute('type', type)
         this.textContent = type === 'password' ? 'Show Password' : 'Hide Password'
     });
 
